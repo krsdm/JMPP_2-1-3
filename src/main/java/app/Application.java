1626d17da59188f8a113857cc5643e9f56280583
krsdm <krsdm@ya.ru>
2021-08-22 23:41:52 +0300

    Task 2.1.3 is done

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,17 +2,18 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        long time = applicationContext.getBean(AnimalsCage.class).getTimer().getTime();
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
+            System.out.println(time == bean.getTimer().getTime());
             bean.whatAnimalSay();
         }
     }
